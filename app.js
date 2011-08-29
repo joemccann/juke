a4ca93ccb4487bc0127e34b46ee65dd707be2a04
Joe McCann <joseph.isaac@gmail.com>
2011-08-29 16:53:35 +0200

    Created strings file; added titles hash to it.

--- app.js
+++ app.js
@@ -39,17 +39,8 @@ if(debug){
 
 var app = module.exports = express.createServer();
 
-// Change these at your will...
-var titles = {
-  defaultDashboardPage: "Dashboard Page",
-  defaultRegistrationPage: "Register for Your Account",
-  redisErrorUsername: "Registration Page Set Username - Redis Error",
-  redisErrorEmail: "Registration Page Set Email - Redis Error",
-  redisErrorSetVerify: "Set Verification State - Redis Error",
-  redisErrorSetUnique: "Set Unique Link - Redis Error",
-  emailErrorSend: "Error Sending Email - SMTP Error",
-  successRegistrationPage: "Registration Page - Success"
-}
+// Change these at your will in the related file.
+var titles = require(__dirname+ '/utils/strings.js')
 
 // Configuration
 
---
+++ utils/strings.js
@@ -0,0 +1,10 @@
+export.titles = {
+  defaultDashboardPage: "Dashboard Page",
+  defaultRegistrationPage: "Register for Your Account",
+  redisErrorUsername: "Registration Page Set Username - Redis Error",
+  redisErrorEmail: "Registration Page Set Email - Redis Error",
+  redisErrorSetVerify: "Set Verification State - Redis Error",
+  redisErrorSetUnique: "Set Unique Link - Redis Error",
+  emailErrorSend: "Error Sending Email - SMTP Error",
+  successRegistrationPage: "Registration Page - Success"
+}
